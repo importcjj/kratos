c6d66e2cfc5d47f3a8d9670646962332524f38e8
Importcjj <importcjj@gmail.com>
2015-09-20 20:34:31 +0800

    new app

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='kratos',
+    version='0.1',
+    description='Backend To Show DouyuTV Statistics',
+    longdescription='Backend To Show DouyuTV Statistics',
+    author='jiaju.chen',
+    author_mail='jiaju.chen@ele.me',
+    requirements=[
+        'Flask==0.10.1'
+    ]
+)
