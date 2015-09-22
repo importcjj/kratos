8e10d2db503c9121866e9566c312bc2d740693e5
Importcjj <importcjj@gmail.com>
2015-09-22 14:07:50 +0800

    add errorhandler

--- kratos/__init__.py
+++ kratos/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,3 +14,8 @@ def ping():
 @app.route('/whoami')
 def whoami():
     return '<h1>Kratos</h1>'
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return flask.render_template('404.html')
